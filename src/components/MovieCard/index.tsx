167d0fcf5d4ae3054890c4314ed46654b2095f25
Torres-ssf <sergio.uft@gmail.com>
2021-01-19 19:06:58 -0300

    feat(movie card): add img tag to display movie poster to MovieCard component

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export const MovieCard: React.FC = ({ children }) => (
-  <Container>{children}</Container>
-);
+export const MovieCard: React.FC = () => {
+  return (
+    <Container>
+      <figure>
+        <img
+          src="https://image.tmdb.org/t/p/w342/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg"
+          alt="movie name"
+        />
+      </figure>
+    </Container>
+  );
+};
--- src/components/MovieCard/styles.ts
+++ src/components/MovieCard/styles.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
-export const Container = styled.article``;
+export const Container = styled.article`
+  border-radius: 5px;
+  margin: 10px;
+
+  figure {
+    img {
+      border-radius: 5px;
+      object-fit: cover;
+      max-width: 100%;
+    }
+  }
+`;
