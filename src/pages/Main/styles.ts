47b70989c84735addeaa63302b6bb62a18260af3
Torres-ssf <sergio.uft@gmail.com>
2021-01-19 20:59:52 -0300

    style(main page): add styles for movie list at Main page

--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -1,13 +1,34 @@
 import styled from 'styled-components';
 
-export const Container = styled.main`
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    list-style: none;
+export const Container = styled.main``;
+
+export const MovieList = styled.ul`
+  display: grid;
+  grid-template-columns: 16.6% 16.6% 16.6% 16.6% 16.6% 16.6%;
+  flex-wrap: wrap;
+  list-style: none;
+
+  @media (max-width: 1650px) {
+    grid-template-columns: 20% 20% 20% 20% 20%;
   }
 
-  li {
-    margin: 10px;
+  @media (max-width: 1380px) {
+    grid-template-columns: 25% 25% 25% 25%;
+  }
+
+  @media (max-width: 1380px) {
+    grid-template-columns: 25% 25% 25% 25%;
+  }
+
+  @media (max-width: 1020px) {
+    grid-template-columns: 33% 33% 33%;
+  }
+
+  @media (max-width: 720px) {
+    grid-template-columns: 50% 50%;
+  }
+
+  @media (max-width: 500px) {
+    grid-template-columns: 100%;
   }
 `;
