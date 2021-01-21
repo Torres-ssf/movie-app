f41d93e15df71a251210f05f25e21084862cf469
Torres-ssf <sergio.uft@gmail.com>
2021-01-20 22:59:53 -0300

    style(movie list navigation): implement change sort type functionality

--- src/components/MovieListNavigation/styles.ts
+++ src/components/MovieListNavigation/styles.ts
@@ -1,4 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface IListItemProps {
+  selected: boolean;
+}
 
 export const Container = styled.nav`
   height: 40px;
@@ -8,11 +12,6 @@ export const Container = styled.nav`
     display: flex;
     border-bottom: 1px solid #3899bf;
     flex: 1;
-
-    li {
-      display: flex;
-      margin-right: 16px;
-    }
   }
 
   button {
@@ -25,4 +24,20 @@ export const Container = styled.nav`
     font-weight: 500;
     text-decoration: none;
   }
+`;
+
+export const ListItem = styled.li<IListItemProps>`
+  display: flex;
+  margin-right: 16px;
+
+  ${props =>
+    props.selected &&
+    css`
+      font-weight: 700;
+      border-bottom: 3px solid #3899bf;
+
+      button {
+        font-weight: 700;
+      }
+    `}
 `;
