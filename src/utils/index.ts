dd910b19a0d32ea3b5061a7274389be0101b5e59
Torres-ssf <sergio.uft@gmail.com>
2021-01-26 20:27:15 -0300

    feat(utils): add to utils function to extract genres array

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { movieGenresObject } from '../constants';
 
-export const formatCardGenresString = (genresArray: number[]): string => {
+export const extractGenresToString = (genresArray: number[]): string => {
   if (genresArray) {
     const genreString = genresArray.map(genre => {
       return movieGenresObject[genre];
@@ -12,11 +12,21 @@ export const formatCardGenresString = (genresArray: number[]): string => {
   return 'Genre not specified';
 };
 
+export const extractGenres = (genresArray: number[]): Array<string> => {
+  return genresArray.map(genre => movieGenresObject[genre]);
+};
+
 export const randomANumber = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-export const formatImageUrl = (path: string, size = 'w342'): string => {
+export const formatImageUrl = ({
+  path,
+  size = 'w342',
+}: {
+  path: string;
+  size?: string;
+}): string => {
   const imageBaseUrl = process.env.REACT_APP_API_IMAGE_URL;
 
   return `${imageBaseUrl}/${size}/${path}`;
